b92752c66a48e36b5437821aa4c0558f1404d1da
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-10-14 14:17:43 -0700

    bug fix

--- molnsutil/molnsutil.py
+++ molnsutil/molnsutil.py
@@ -687,8 +687,7 @@ class DistributedEnsemble():
             self.c = IPython.parallel.Client()
         else:
             self.c = client
-        #self.c[:].use_dill()
-        self.c[:].use_cloudpickle()
+        self.c[:].use_dill()
         self.dv = self.c[:]
         self.lv = self.c.load_balanced_view()
 
