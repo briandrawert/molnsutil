9a564da7bf93f28a4090d963e26f9d20ee0f7f1e
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-10-15 11:12:43 -0700

    bug fix

--- molnsutil/molnsutil.py
+++ molnsutil/molnsutil.py
@@ -548,7 +548,8 @@ class DistributedEnsemble():
                 pparams.extend( [param]*num_chunks )
                 presult_list.extend(self.result_list[id])
             # Run mapper & aggregator
-            results = self.lv.map_async(map_and_aggregate, presult_list, [mapper]*num_pchunks,[aggregator]*num_pchunks,[cache_results]*num_pchunks)
+            #def map_and_aggregate(results, param_set_id, mapper, aggregator=None, cache_results=False):
+            results = self.lv.map_async(map_and_aggregate, presult_list, param_set_ids, [mapper]*num_pchunks,[aggregator]*num_pchunks,[cache_results]*num_pchunks)
         else:
             # If we don't store the realizations (or use the stored ones)
             if chunk_size is None:
