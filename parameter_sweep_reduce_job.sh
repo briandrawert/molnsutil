5f06cc73211d43a5023718fea58ffbac5728936e
Aviral Takkar <aviraltakkar@gmail.com>
2016-11-04 15:41:33 -0700

    remove sudo, assuming user is part of the docker group on the cluster in use

--- parameter_sweep_reduce_job.sh
+++ parameter_sweep_reduce_job.sh
@@ -7,7 +7,7 @@ run_reducer()
   c_dir=$2
   molnsutil_dir=`dirname ${c_dir}`
 
-  COMMAND="sudo docker run -e PYTHONPATH=/stochss-master/app/lib -e STOCHKIT_HOME=/stochss-master/StochKit --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name `basename ${1}` aviralcse/stochss_qsub python parameter_sweep_run_reducer.py ${job_working_directory}"
+  COMMAND="docker run -e PYTHONPATH=/stochss-master/app/lib -e STOCHKIT_HOME=/stochss-master/StochKit --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name `basename ${1}` aviralcse/stochss_qsub python parameter_sweep_run_reducer.py ${job_working_directory}"
 
   ${COMMAND}
   touch ${c_dir}/complete
