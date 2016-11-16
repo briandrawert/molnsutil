a97f0c130e62f4bf6039bce915092da111ffeec8
Aviral Takkar <aviraltakkar@gmail.com>
2016-11-15 16:29:22 -0800

    bash fixes

--- parameter_sweep_reduce_job.sh
+++ parameter_sweep_reduce_job.sh
@@ -6,7 +6,7 @@ run_reducer()
   job_working_directory=`pwd`
   c_dir=$2
   molnsutil_dir=`dirname ${c_dir}`
-  container_name=`basename ${1}`
+  container_name=$1
 
   COMMAND="docker run -e PYTHONPATH=/stochss-master/app/lib -e STOCHKIT_HOME=/stochss-master/StochKit -e C_INCLUDE_PATH=/usr/lib/openmpi/include -e INSTANT_SYSTEM_CALL_METHOD=SUBPROCESS --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name ${container_name} aviralcse/stochss_qsub python parameter_sweep_run_reducer.py ${job_working_directory}"
 
