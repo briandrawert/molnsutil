1411203a672930bf6c88e957a49d603ef934c5d8
Aviral Takkar <aviraltakkar@gmail.com>
2016-11-17 16:48:06 -0800

    fix bash errors

--- parameter_sweep_reduce_job.sh
+++ parameter_sweep_reduce_job.sh
@@ -8,10 +8,12 @@ run_reducer()
   molnsutil_dir=`dirname ${c_dir}`
   container_name=$1
 
-  COMMAND="docker pull aviralcse/stochss_qsub; docker run -e PYTHONPATH=/stochss-master/app/lib -e STOCHKIT_HOME=/stochss-master/StochKit -e C_INCLUDE_PATH=/usr/lib/openmpi/include -e INSTANT_SYSTEM_CALL_METHOD=SUBPROCESS --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name ${container_name} aviralcse/stochss_qsub python parameter_sweep_run_reducer.py ${job_working_directory}"
+  COMMAND1="docker pull aviralcse/stochss_qsub"
+  COMMAND2="docker run -e PYTHONPATH=/stochss-master/app/lib -e STOCHKIT_HOME=/stochss-master/StochKit -e C_INCLUDE_PATH=/usr/lib/openmpi/include -e INSTANT_SYSTEM_CALL_METHOD=SUBPROCESS --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name ${container_name} aviralcse/stochss_qsub python parameter_sweep_run_reducer.py ${job_working_directory}"
 
-  ${COMMAND}
-  echo ${COMMAND} > ${c_dir}/complete
+  ${COMMAND1}
+  ${COMMAND2}
+  echo ${COMMAND2} > ${c_dir}/complete
 
   DEL_COMMAND="docker rm -f ${container_name}"
 
