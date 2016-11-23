681a7ffda0e6b6ace6b39c973ef2eb774774a294
Aviral Takkar <aviraltakkar@gmail.com>
2016-11-23 10:16:38 -0800

    fix docker run command

--- parameter_sweep_reduce_job.sh
+++ parameter_sweep_reduce_job.sh
@@ -9,7 +9,7 @@ run_reducer()
   container_name=$1
 
   COMMAND1="docker pull aviralcse/stochss_qsub"
-  COMMAND2="docker run -e PYTHONPATH=/stochss-master/app/lib:/stochss-master/app/handlers:/stochss-master/app/ -e STOCHKIT_HOME=/stochss-master/StochKit -e C_INCLUDE_PATH=/usr/lib/openmpi/include -e INSTANT_SYSTEM_CALL_METHOD=SUBPROCESS --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name ${container_name} aviralcse/stochss_qsub \"touch /stochss-master/app/handlers/parametersweep_qsub.py; python parameter_sweep_run_reducer.py ${job_working_directory}\""
+  COMMAND2="docker run -e PYTHONPATH=/stochss-master/app/lib:/stochss-master/app/handlers:/stochss-master/app/ -e STOCHKIT_HOME=/stochss-master/StochKit -e C_INCLUDE_PATH=/usr/lib/openmpi/include -e INSTANT_SYSTEM_CALL_METHOD=SUBPROCESS --volume ${c_dir}:/stochss-master/app/lib/w_dir/ --volume ${molnsutil_dir}:/stochss-master/app/lib/molnsutil/ -w /stochss-master/app/lib/w_dir/ --name ${container_name} aviralcse/stochss_qsub /bin/bash -c \"touch /stochss-master/app/handlers/parametersweep_qsub.py; python parameter_sweep_run_reducer.py ${job_working_directory}\""
 
   #${COMMAND1}
   ${COMMAND2}
