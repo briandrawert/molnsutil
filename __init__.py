1d8a9f66bff753d2705ef7ca4dc2a63fd913ae2d
Aviral Takkar <aviraltakkar@gmail.com>
2016-12-01 17:35:36 -0800

    debugging

--- __init__.py
+++ __init__.py
@@ -696,6 +696,9 @@ class DistributedEnsemble:
 
     def _determine_chunk_size(self, number_of_trajectories):
         """ Determine a optimal chunk size. """
+        self.log.write_log("self.num_engines = {0}, type(self.engines) = {1}, self.num_engines is 'None' = {3}".format(
+            self.num_engines, type(self.num_engines), self.num_engines is 'None'
+        ))
         self.log.write_log("self.num_engines = {0}, type(self.num_engines) = {1}".format(self.num_engines,
                                                                                          type(self.num_engines)))
         if self.num_engines is None or self.num_engines is 'None':
@@ -937,7 +940,11 @@ class ParameterSweep(DistributedEnsemble):
 
     def _determine_chunk_size(self, number_of_trajectories):
         """ Determine a optimal chunk size. """
-        if self.num_engines is None or self.num_engines is 'None':
+        self.log.write_log("self.num_engines = {0}, type(self.engines) = {1}, self.num_engines is 'None' = {3}".format(
+            self.num_engines, type(self.num_engines), self.num_engines is 'None'
+        ))
+
+        if self.num_engines is None or self.num_engines is "None":
             self.log.write_log("self.num_engines is None, returning chunk_size = 1")
             return 1
         num_params = len(self.parameters)
